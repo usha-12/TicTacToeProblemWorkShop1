159f45f30f0586e9256aafc90869f894a1257e6f
usha-12 <usha12ahirwar@gmail.com>
2023-01-16 22:39:14 +0530

    Added: create game board

--- src/com/brideglabz/tictactoe/TicTacToe.java
+++ src/com/brideglabz/tictactoe/TicTacToe.java
@@ -1,7 +1,25 @@
 package com.brideglabz.tictactoe;
 public class TicTacToe {
+    char gameBoard[] = null;
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
+        TicTacToe gameObj = new TicTacToe();
+        gameObj.initialiseGame();
+    }
+    void initialiseGame(){
+        gameBoard = new char[10];
+        for(int index = 0; index < 10; index++)
+            gameBoard[index] = ' ';
     }
 
 }
+/*As a Player would like to
+start fresh by creating a tic-tac-toe board - Create a TicTacToeGame class - Create method for every use case
+and call from main
+- Create a board of char[] of size 10
+and assign empty space to each
+element
+
+-
+0th index is ignored to make it
+ user-friendly*/
