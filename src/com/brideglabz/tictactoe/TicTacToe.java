c45cb9d421f208a6acc098ed95958a65849e278d
usha-12 <usha12ahirwar@gmail.com>
2023-01-16 22:45:17 +0530

    Added: Player chooses to play X or O

--- src/com/brideglabz/tictactoe/TicTacToe.java
+++ src/com/brideglabz/tictactoe/TicTacToe.java
@@ -1,25 +1,48 @@
 package com.brideglabz.tictactoe;
+
+import java.util.Scanner;
+
 public class TicTacToe {
     char gameBoard[] = null;
+    char playerLetter = '\0';
+    char computerLetter = '\0';
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
         TicTacToe gameObj = new TicTacToe();
         gameObj.initialiseGame();
+        gameObj.choosePlayerLetter();
     }
     void initialiseGame(){
         gameBoard = new char[10];
         for(int index = 0; index < 10; index++)
             gameBoard[index] = ' ';
     }
+    void choosePlayerLetter() {
+        System.out.println("\nPlayer please choose your play Letter.");
+        System.out.println("Enter 'X' to play 'X' on your turn.");
+        System.out.println("Or Enter 'O' to play 'O' on your turn.");
+        Scanner sc = new Scanner(System.in);
+        char playerInput = sc.next().charAt(0);
+        sc.close();
+        if (playerInput == 'X' || playerInput == 'x') {
+            playerLetter = 'X';
+            computerLetter = 'O';
+        }
+        else if (playerInput == 'O' || playerInput == 'o') {
+            playerLetter = 'O';
+            computerLetter = 'o';
+        }
+        else
+            System.out.println("\nInvalid Input.\nPlease try again!");
+    }
 
 }
-/*As a Player would like to
-start fresh by creating a tic-tac-toe board - Create a TicTacToeGame class - Create method for every use case
-and call from main
-- Create a board of char[] of size 10
-and assign empty space to each
-element
+/*Ability to allow the
+player to choose a
+letter X or O
 
--
-0th index is ignored to make it
- user-friendly*/
+- Create a method to allow player to
+input X or O and call from main
+- Determine Player and Computer
+
+ Letter to play the game*/
